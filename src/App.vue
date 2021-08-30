30339adafd1e66f5754cecbe68525dabfa9306f1
Duyb <1255248666@qq.com>
2021-08-30 16:13:11 +0800

    ts vue component

--- src/App.vue
+++ src/App.vue
@@ -5,4 +5,42 @@
   </div>
 </template>
 
+<script lang="ts">
+import Vue from 'vue';
+// import Component from 'vue-class-component';
+
+export default Vue.extend({
+  data() {
+    return {
+      name: 'DU',
+      message: 'Hello TS',
+    };
+  },
+  methods: {
+    test() {
+      this.name;
+    },
+  },
+});
+
+// @Component
+// export default class App extends Vue {
+//   name = 'DU';
+//   message = 'Hello vue-class-component';
+
+//   // computed
+//   get msg(): string {
+//     return 'Hi, ' + this.name;
+//   }
+//   created(): void {
+//     console.log('app created');
+//     this.test();
+//   }
+
+//   test(): void {
+//     console.log(this.name);
+//   }
+// }
+</script>
+
 <style lang="scss" scoped></style>
